003551f9fe053db532c14a1703310007d2910a1b
Yago Torigoe <japafivem@gmail.com>
2026-05-14 00:13:15 -0300

    Adicionando variaveis de autenticacao no JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const usuarioCorreto = "aluno@fiap.com.br";
+const senhaCorreta = "1234";
+
+const loginSection = document.getElementById('login-section');
+const loginForm = document.getElementById('login-form');
