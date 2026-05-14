5ecb4544d2c0bf3c253a6745988cce30e68238ad
Yago Torigoe <japafivem@gmail.com>
2026-05-14 00:22:27 -0300

    Finalizando logica de manipulacao de imagem

--- script.js
+++ script.js
@@ -31,3 +31,27 @@ btnPrompt.addEventListener('click', function() {
         alert(`Obrigado pelo feedback! Você disse: "${resposta}".`);
     }
 });
+
+const slideImg = document.getElementById('slide-img');
+const btnNext = document.getElementById('btn-next');
+
+// Array com os links das imagens de gatos
+const imagensSlideshow = [
+    "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=600&h=400&fit=crop",
+    "https://images.unsplash.com/photo-1495360010541-f48722b34f7d?w=600&h=400&fit=crop",
+    "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?w=600&h=400&fit=crop",
+    "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?w=600&h=400&fit=crop"
+];
+
+let indiceAtual = 0;
+
+btnNext.addEventListener('click', function() {
+    indiceAtual++;
+    
+    // Se chegar na última foto, volta para a primeira
+    if(indiceAtual >= imagensSlideshow.length) {
+        indiceAtual = 0;
+    }
+    
+    slideImg.src = imagensSlideshow[indiceAtual];
+});
