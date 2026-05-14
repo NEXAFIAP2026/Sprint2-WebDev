c53dde75a4051dbf7e7e06f5e7db01d4b8327693
Yago Torigoe <japafivem@gmail.com>
2026-05-14 00:14:09 -0300

    Validacao de login e manipulacao de strings

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const senhaCorreta = "1234";
 
 const loginSection = document.getElementById('login-section');
 const loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById('email').value;
+    const passwordInput = document.getElementById('password').value;
+
+    if(emailInput === usuarioCorreto && passwordInput === senhaCorreta) {
+        alert("Login efetuado com sucesso!");
+        
+        loginSection.style.display = 'none';
+        document.getElementById('app-section').style.display = 'block';
+
+        let nomeUsuario = emailInput.split('@')[0].toUpperCase();
+        document.getElementById('welcome-message').textContent = `Bem-vindo(a), ${nomeUsuario}!`;
+    } else {
+        alert("E-mail ou senha incorretos.");
+    }
+});
